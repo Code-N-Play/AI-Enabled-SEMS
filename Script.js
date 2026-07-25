a4901d17b7786b1a3f103cb7586bab5cb058af67
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-07-25 16:44:24 +0530

    main

---
+++ Script.js
@@ -0,0 +1,41 @@
+import express from 'express'
+
+const app = express()
+app.set ("view engine", "ejs");
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.render("home")
+})
+
+app.get('/EnergyMonitoring', (req, res) => {
+    res.render("EnergyMonitoring")
+})
+
+app.get('/SamrtSockets', (req, res) => {
+    res.render("SamrtSockets")
+})
+
+app.get('/Rooms', (req, res) => {
+    res.render("Rooms")
+})
+  
+app.get('/AiInsights', (req, res) => {
+      res.render("AiInsights")
+})
+  
+app.get('/Analytics', (req, res) => {
+      res.render("Analytics")
+})
+
+app.get('/Alerts', (req, res) => {
+    res.render("Alerts")
+})
+
+app.get('/Settings', (req, res) => {
+    res.render("Settings")
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
